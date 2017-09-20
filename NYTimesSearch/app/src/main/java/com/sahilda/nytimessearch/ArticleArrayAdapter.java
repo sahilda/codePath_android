11ba470060954510dc5ad696db7b7ebbf401fc54
Sahil Agarwal <sahil.agarwal@xoom.com>
2017-09-20 14:12:56 -0700

    Added NYTimes Search article project and completed video tutorial

---
+++ NYTimesSearch/app/src/main/java/com/sahilda/nytimessearch/ArticleArrayAdapter.java
@@ -0,0 +1,48 @@
+package com.sahilda.nytimessearch;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ArticleArrayAdapter extends ArrayAdapter<Article> {
+
+    public ArticleArrayAdapter(Context context, List<Article> articles) {
+        super(context, android.R.layout.simple_list_item_1, articles);
+    }
+
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Article article = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_article_result, parent, false);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        tvTitle.setText(article.getHeadline());
+
+        if (!TextUtils.isEmpty(article.getThumbNail())) {
+            Picasso.with(getContext())
+                    .load(article.getThumbNail())
+                    .into(imageView);
+        }
+
+        return convertView;
+    }
+}
---
+++ NYTimesSearch/app/src/main/java/com/sahilda/nytimessearch/activities/ArticleActivity.java
@@ -0,0 +1,29 @@
+package com.sahilda.nytimessearch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.sahilda.nytimessearch.Article;
+import com.sahilda.nytimessearch.R;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        Article article = (Article) getIntent().getSerializableExtra("article");
+        WebView webView = (WebView) findViewById(R.id.wvArticle);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(article.getWebUrl());
+    }
+}
