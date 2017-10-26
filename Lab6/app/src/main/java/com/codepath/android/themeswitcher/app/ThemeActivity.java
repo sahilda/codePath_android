75840ffd4b051ac290974eb4fa7f0ace2154272b
Sahil Agarwal <sahildagarwal@gmail.com>
2017-10-26 16:14:03 -0700

    Completed Lab 6

--- Lab6/app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ Lab6/app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -4,13 +4,41 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class ThemeActivity extends AppCompatActivity {
+
+    private Spinner spThemes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_theme);
+        setupSpinnerItemSelection();
+    }
+
+    private void setupSpinnerItemSelection() {
+        spThemes = (Spinner)  findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentPosition);
+        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
+
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (ThemeApplication.currentPosition != position) {
+                    Utils.changeToTheme(ThemeActivity.this, position);
+                }
+                ThemeApplication.currentPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -34,4 +62,5 @@ public class ThemeActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ Lab6/app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,9 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+public class ThemeApplication extends Application {
+
+    public static int currentPosition;
+
+}
